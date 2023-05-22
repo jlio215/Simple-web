1734a665a17fe380b4446f4881cf6552f4b19d40
jlio215 <31058099+jlio215@users.noreply.github.com>
2023-05-22 10:37:15 -0500

    first commit

---
+++ restaurants.js
@@ -0,0 +1,27 @@
+var restaurants = [
+    {
+        nombre:"Pacifico Mar",
+        direccion: "Calle 1",
+        tipoComida: "Comida de mar",
+        imagen: "iphone.jpg"
+    },
+    {
+        nombre:"Pacifico Mar",
+        direccion: "Calle 1",
+        tipoComida: "Comida de mar",
+        imagen: "iphone.jpg"
+    },
+    {
+        nombre:"Pacifico Mar",
+        direccion: "Calle 1",
+        tipoComida: "Comida de mar",
+        imagen: "iphone.jpg"
+    },
+    {
+        nombre:"Pacifico Mar",
+        direccion: "Calle 1",
+        tipoComida: "Comida de mar",
+        imagen: "iphone.jpg"
+    },
+
+]
