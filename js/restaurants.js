cfde2ee2f161099ec2d41ce8a29398a39ec2b8bf
jlio215 <31058099+jlio215@users.noreply.github.com>
2023-05-27 11:29:17 -0500

    workhome 2

---
+++ js/restaurants.js
@@ -0,0 +1,27 @@
+var restaurants = [
+    {
+        nombre:"Pacifico Mar",
+        direccion: "Calle 1",
+        tipoComida: "Sea Food",
+        imagen: "img/ingredientes-comida-de-mar-parrilla.jpg"
+    },
+    {
+        nombre:"Italian Food",
+        direccion: "Calle 2",
+        tipoComida: "Italian Food",
+        imagen: "img/Italian.jpg"
+    },
+    {
+        nombre:"Mexican Food",
+        direccion: "Calle 3",
+        tipoComida: "Mexican Food",
+        imagen: "img/mexico.webp"
+    },
+    {
+        nombre:"Carbon Food",
+        direccion: "Calle 4",
+        tipoComida: "All types of meat",
+        imagen: "img/ribEye.jpg"
+    },
+
+]
